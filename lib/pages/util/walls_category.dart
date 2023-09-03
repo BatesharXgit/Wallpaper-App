30308adc541beddcde68c3ff8ffe7971ae540851
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-03 11:33:12 +0530

    v 1.0.0

--- lib/pages/util/walls_category.dart
+++ lib/pages/util/walls_category.dart
@@ -1,4 +1,33 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:luca_ui/pages/util/homepage.dart';
+
+final FirebaseStorage storage = FirebaseStorage.instance;
+final Reference amoledRef = storage.ref().child('wallpaper');
+final Reference landscapesRef = storage.ref().child('wallpaper');
+final Reference cityscapesRef = storage.ref().child('wallpaper');
+final Reference spaceRef = storage.ref().child('wallpaper');
+final Reference stockRef = storage.ref().child('wallpaper');
+final Reference minimalistRef = storage.ref().child('wallpaper');
+final Reference natureRef = storage.ref().child('wallpaper');
+final Reference animalsRef = storage.ref().child('wallpaper');
+final Reference scifiRef = storage.ref().child('wallpaper');
+final Reference gamesRef = storage.ref().child('wallpaper');
+
+List<Reference> amoledRefs = [];
+List<Reference> landscapesRefs = [];
+List<Reference> cityscapesRefs = [];
+List<Reference> spaceRefs = [];
+List<Reference> stockRefs = [];
+List<Reference> minimalistRefs = [];
+List<Reference> natureRefs = [];
+List<Reference> animalsRefs = [];
+List<Reference> scifiRefs = [];
+List<Reference> gamesRefs = [];
+
+ScrollController scrollController = ScrollController();
 
 //=====================================================================================================================
 //===============================================  Amoled Wallpaper ===================================================
@@ -12,8 +41,145 @@ class AmoledWallpaper extends StatefulWidget {
 
 class _AmoledWallpaperState extends State<AmoledWallpaper> {
   @override
+  void initState() {
+    super.initState();
+    loadamoledImages();
+  }
+
+  Future<void> loadamoledImages() async {
+    final ListResult result = await amoledRef.listAll();
+    amoledRefs = result.items.toList();
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Color backgroundColor = Theme.of(context).colorScheme.background;
+    Color primaryColor = Theme.of(context).colorScheme.primary;
+    // Color secondaryColor = Theme.of(context).colorScheme.secondary;
+    // Color tertiaryColor = Theme.of(context).colorScheme.tertiary;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+        title: Text(
+          'Amoled',
+          style: GoogleFonts.orbitron(
+            color: primaryColor,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder<ListResult>(
+                future: amoledRef.listAll(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return _buildPlaceholder();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (snapshot.hasData &&
+                      snapshot.data!.items.isNotEmpty) {
+                    List<Reference> imageRefs = snapshot.data!.items;
+
+                    return GridView.builder(
+                      physics: BouncingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: amoledRefs.length,
+                      itemBuilder: (context, index) {
+                        final amoRef = imageRefs[index];
+                        return FutureBuilder<String>(
+                          future: amoRef.getDownloadURL(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return _buildCircularIndicator();
+                            } else if (snapshot.hasError) {
+                              return _buildErrorWidget();
+                            } else if (snapshot.hasData) {
+                              return _buildImageWidget(snapshot.data!);
+                            } else {
+                              return Container();
+                            }
+                          },
+                        );
+                      },
+                    );
+                  } else {
+                    return Center(child: Text('No images available'));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImageWidget(String imageUrl) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FullScreenImagePage(imageUrl: imageUrl),
+          ),
+        );
+      },
+      child: Hero(
+        tag: imageUrl,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: LocationListItem(
+              imageUrl: imageUrl,
+              scrollController: scrollController,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlaceholder() {
+    return Center(
+      child: LoadingAnimationWidget.newtonCradle(
+        size: 35,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+    );
+  }
+
+  Widget _buildErrorWidget() {
+    return Container(
+      color: Colors.transparent,
+      child: const Icon(
+        Icons.error,
+        color: Colors.red,
+      ),
+    );
+  }
+
+  Widget _buildCircularIndicator() {
+    return Center(
+      child: LoadingAnimationWidget.fallingDot(
+        size: 35,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+    );
   }
 }
 
